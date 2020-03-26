f48b97ac2ee9f397625eb8d0341f1c1c6fdfcd7c
wryang <yang@yang-mac.local>
2020-03-26 12:29:21 +0900

    fnf 데이터 소스 사용 샘플 작업중...
    insert / delete  가능
    update ing....

---
+++ src/main/java/operato/fnf/wcs/entity/WmsMheHr.java
@@ -0,0 +1,251 @@
+package operato.fnf.wcs.entity;
+
+import java.util.Date;
+
+import org.springframework.context.annotation.Primary;
+
+import xyz.elidom.dbist.annotation.Column;
+import xyz.elidom.dbist.annotation.ColumnType;
+import xyz.elidom.dbist.annotation.DataSourceType;
+import xyz.elidom.dbist.annotation.GenerationRule;
+import xyz.elidom.dbist.annotation.Ignore;
+import xyz.elidom.dbist.annotation.Index;
+import xyz.elidom.dbist.annotation.PrimaryKey;
+import xyz.elidom.dbist.annotation.Table;
+
+@Table(name = "mhe_hr", idStrategy = GenerationRule.UUID, dataSourceType=DataSourceType.DATASOURCE, uniqueFields="whCd,workUnit", indexes = {
+	@Index(name = "mhe_hr_id01", columnList = "wh_cd,wave_no", unique = false)
+})
+public class WmsMheHr extends xyz.elidom.orm.entity.basic.AbstractStamp {
+	
+	/**
+	 * SerialVersion UID
+	 */
+	private static final long serialVersionUID = -8555254391446018815L;
+
+	@Ignore
+	private String id;
+
+	@PrimaryKey
+	@Column (name = "wh_cd", nullable = false, length = 20)
+	private String whCd;
+
+	@Column (name = "strr_id", nullable = false, length = 20)
+	private String strrId;
+
+	@PrimaryKey
+	@Column (name = "work_date", nullable = false, length = 8)
+	private String workDate;
+  
+	@Column (name = "work_unit", nullable = false, length = 20)
+	private String workUnit;
+	
+	@Column (name = "biz_type", length = 10)
+	private String bizType;
+	
+	@Column (name = "mhe_no", length = 10)
+	private String mheNo;
+
+	@Column (name = "wave_no", length = 20)
+	private String waveNo;
+
+	@Column (name = "workseq_no", length = 20)
+	private String workseqNo;
+	
+	@Column (name = "pick_qty", length = 10)
+	private Integer pickQty;
+	
+	@Column (name = "cmpt_qty", length = 10)
+	private Integer cmptQty;
+	
+	@Column (name = "status", length = 1)
+	private String status;
+	
+	@Column (name = "rcv_datetime", type = ColumnType.DATETIME)
+	private Date rcvDatetime;
+	
+	@Column (name = "cnf_datetime", type = ColumnType.DATETIME)
+	private Date cnfDatetime;
+	
+	@Column (name = "end_datetime", type = ColumnType.DATETIME)
+	private Date endDatetime;
+
+	@Column (name = "ins_datetime", type = ColumnType.DATETIME)
+	private Date insDatetime;
+	
+	@Column (name = "ins_person_id", length = 20)
+	private String insPersonId;
+
+	@Column (name = "descr", length = 200)
+	private String descr;
+
+	@Column (name = "prcs_yn", length = 1)
+	private String prcsYn;
+
+	@Column (name = "prcs_datetime", type = ColumnType.DATETIME)
+	private Date prcsDatetime;
+	
+	@Column (name = "pas_chute_tcd", length = 10)
+	private String pasChuteTcd;
+
+	public String getWhCd() {
+		return whCd;
+	}
+
+	public void setWhCd(String whCd) {
+		this.whCd = whCd;
+	}
+
+	public String getStrrId() {
+		return strrId;
+	}
+
+	public void setStrrId(String strrId) {
+		this.strrId = strrId;
+	}
+
+	public String getWorkDate() {
+		return workDate;
+	}
+
+	public void setWorkDate(String workDate) {
+		this.workDate = workDate;
+	}
+
+	public String getWorkUnit() {
+		return workUnit;
+	}
+
+	public void setWorkUnit(String workUnit) {
+		this.workUnit = workUnit;
+	}
+
+	public String getBizType() {
+		return bizType;
+	}
+
+	public void setBizType(String bizType) {
+		this.bizType = bizType;
+	}
+
+	public String getMheNo() {
+		return mheNo;
+	}
+
+	public void setMheNo(String mheNo) {
+		this.mheNo = mheNo;
+	}
+
+	public String getWaveNo() {
+		return waveNo;
+	}
+
+	public void setWaveNo(String waveNo) {
+		this.waveNo = waveNo;
+	}
+
+	public String getWorkseqNo() {
+		return workseqNo;
+	}
+
+	public void setWorkseqNo(String workseqNo) {
+		this.workseqNo = workseqNo;
+	}
+
+	public Integer getPickQty() {
+		return pickQty;
+	}
+
+	public void setPickQty(Integer pickQty) {
+		this.pickQty = pickQty;
+	}
+
+	public Integer getCmptQty() {
+		return cmptQty;
+	}
+
+	public void setCmptQty(Integer cmptQty) {
+		this.cmptQty = cmptQty;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getRcvDatetime() {
+		return rcvDatetime;
+	}
+
+	public void setRcvDatetime(Date rcvDatetime) {
+		this.rcvDatetime = rcvDatetime;
+	}
+
+	public Date getCnfDatetime() {
+		return cnfDatetime;
+	}
+
+	public void setCnfDatetime(Date cnfDatetime) {
+		this.cnfDatetime = cnfDatetime;
+	}
+
+	public Date getEndDatetime() {
+		return endDatetime;
+	}
+
+	public void setEndDatetime(Date endDatetime) {
+		this.endDatetime = endDatetime;
+	}
+
+	public Date getInsDatetime() {
+		return insDatetime;
+	}
+
+	public void setInsDatetime(Date insDatetime) {
+		this.insDatetime = insDatetime;
+	}
+
+	public String getInsPersonId() {
+		return insPersonId;
+	}
+
+	public void setInsPersonId(String insPersonId) {
+		this.insPersonId = insPersonId;
+	}
+
+	public String getDescr() {
+		return descr;
+	}
+
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+
+	public String getPrcsYn() {
+		return prcsYn;
+	}
+
+	public void setPrcsYn(String prcsYn) {
+		this.prcsYn = prcsYn;
+	}
+
+	public Date getPrcsDatetime() {
+		return prcsDatetime;
+	}
+
+	public void setPrcsDatetime(Date prcsDatetime) {
+		this.prcsDatetime = prcsDatetime;
+	}
+
+	public String getPasChuteTcd() {
+		return pasChuteTcd;
+	}
+
+	public void setPasChuteTcd(String pasChuteTcd) {
+		this.pasChuteTcd = pasChuteTcd;
+	}
+
+}
---
+++ src/main/java/operato/fnf/wcs/rest/WnsMheHrController.java
@@ -0,0 +1,89 @@
+package operato.fnf.wcs.rest;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import operato.fnf.wcs.entity.WmsMheHr;
+import xyz.elidom.dbist.dml.Page;
+import xyz.elidom.orm.IQueryManager;
+import xyz.elidom.orm.manager.DataSourceManager;
+import xyz.elidom.orm.system.annotation.service.ApiDesc;
+import xyz.elidom.orm.system.annotation.service.ServiceDesc;
+import xyz.elidom.sys.system.service.DynamicDataSourceService;
+import xyz.elidom.util.BeanUtil;
+
+@RestController
+@Transactional
+@ResponseStatus(HttpStatus.OK)
+@RequestMapping("/rest/wms_mhe_hr")
+@ServiceDesc(description = "WMS MHE HR Service API")
+public class WnsMheHrController extends DynamicDataSourceService {
+
+	@Override
+	protected Class<?> entityClass() {
+		return WmsMheHr.class;
+	}
+	
+	@Override
+	protected IQueryManager queryManager() {
+		return BeanUtil.get(DataSourceManager.class).getQueryManager(this.entityClass());
+	}
+
+
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description = "Search (Pagination) By Search Conditions")
+	public Page<?> index(@RequestParam(name = "page", required = false) Integer page,
+			@RequestParam(name = "limit", required = false) Integer limit,
+			@RequestParam(name = "select", required = false) String select,
+			@RequestParam(name = "sort", required = false) String sort,
+			@RequestParam(name = "query", required = false) String query) {
+		return this.search(this.entityClass(), page, limit, select, sort, query);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description = "Find one by ID")
+	public WmsMheHr findOne(@PathVariable("id") String id) {
+		return this.getOne(this.entityClass(), id);
+	}
+
+	@RequestMapping(value = "/{id}/exist", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description = "Check exists By ID")
+	public Boolean isExist(@PathVariable("id") String id) {
+		return this.isExistOne(this.entityClass(), id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	@ApiDesc(description = "Create")
+	public WmsMheHr create(@RequestBody WmsMheHr input) {
+		return this.createOne(input);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description = "Update")
+	public WmsMheHr update(@PathVariable("id") String id, @RequestBody WmsMheHr input) {
+		return this.updateOne(input);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description = "Delete")
+	public void delete(@PathVariable("id") String id) {
+		this.deleteOne(this.entityClass(), id);
+	}
+
+	@RequestMapping(value = "/update_multiple", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description = "Create, Update or Delete multiple at one time")
+	public Boolean multipleUpdate(@RequestBody List<WmsMheHr> list) {
+		return this.cudMultipleData(this.entityClass(), list);
+	}
+}

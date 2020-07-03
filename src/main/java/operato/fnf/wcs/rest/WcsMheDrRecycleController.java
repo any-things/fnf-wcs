2a861060f885a413d3ad4f6b0064ea754111e5d0
chunseop <chunseop@gmail.com>
2020-07-03 23:39:36 +0900

    회수

---
+++ src/main/java/operato/fnf/wcs/rest/WcsMheDrRecycleController.java
@@ -0,0 +1,84 @@
+package operato.fnf.wcs.rest;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import operato.fnf.wcs.entity.WcsMheDrRecycle;
+
+import xyz.elidom.orm.system.annotation.service.ApiDesc;
+import xyz.elidom.orm.system.annotation.service.ServiceDesc;
+import xyz.elidom.sys.system.service.AbstractRestService;
+import xyz.elidom.dbist.dml.Page;
+
+@RestController
+@Transactional
+@ResponseStatus(HttpStatus.OK)
+@RequestMapping("/rest/mhe_dr_recycle")
+@ServiceDesc(description="WcsMheDrRecycle Service API")
+public class WcsMheDrRecycleController extends AbstractRestService {
+
+	@Override
+	protected Class<?> entityClass() {
+		return WcsMheDrRecycle.class;
+	}
+  
+	@RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description="Search (Pagination) By Search Conditions")  
+	public Page<?> index(
+		@RequestParam(name="page", required=false) Integer page, 
+		@RequestParam(name="limit", required=false) Integer limit, 
+		@RequestParam(name="select", required=false) String select, 
+		@RequestParam(name="sort", required=false) String sort,
+		@RequestParam(name="query", required=false) String query) {   
+		return this.search(this.entityClass(), page, limit, select, sort, query);
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description="Find one by ID")
+	public WcsMheDrRecycle findOne(@PathVariable("id") String id) {
+		return this.getOne(this.entityClass(), id);
+	}
+
+	@RequestMapping(value="/{id}/exist", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description="Check exists By ID")
+	public Boolean isExist(@PathVariable("id") String id) {
+		return this.isExistOne(this.entityClass(), id);
+	}
+
+	@RequestMapping(method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	@ApiDesc(description="Create")
+	public WcsMheDrRecycle create(@RequestBody WcsMheDrRecycle input) {
+		return this.createOne(input);
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description="Update")
+	public WcsMheDrRecycle update(@PathVariable("id") String id, @RequestBody WcsMheDrRecycle input) {
+		return this.updateOne(input);
+	}
+  
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description="Delete")
+	public void delete(@PathVariable("id") String id) {
+		this.deleteOne(this.entityClass(), id);
+	}  
+  
+	@RequestMapping(value="/update_multiple", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ApiDesc(description="Create, Update or Delete multiple at one time")
+	public Boolean multipleUpdate(@RequestBody List<WcsMheDrRecycle> list) {
+		return this.cudMultipleData(this.entityClass(), list);
+	}
+
+	  
+	}

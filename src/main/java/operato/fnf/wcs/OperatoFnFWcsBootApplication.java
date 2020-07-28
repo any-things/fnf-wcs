3595b3c6900872f033635efc1fbd5af48f61c356
hatiolab <shortstop@hatiolab.com>
2020-07-28 17:03:47 +0900

    Job Scheduler를 별도 스레드로 처리하도록 수정

--- src/main/java/operato/fnf/wcs/OperatoFnFWcsBootApplication.java
+++ src/main/java/operato/fnf/wcs/OperatoFnFWcsBootApplication.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @EnableAsync(proxyTargetClass = true)
 @EnableScheduling
@@ -14,9 +17,21 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 @ComponentScan(basePackages = { "xyz.anythings.*", "xyz.elidom.*", "operato.*" })
 @ImportResource({ "classpath:/WEB-INF/application-context.xml", "classpath:/WEB-INF/dataSource-context.xml" })
-public class OperatoFnFWcsBootApplication {
+public class OperatoFnFWcsBootApplication implements SchedulingConfigurer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(OperatoFnFWcsBootApplication.class, args);
 	}
+	
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		ThreadPoolTaskScheduler poolScheduler = new ThreadPoolTaskScheduler();
+		poolScheduler.setBeanName("FnfPool");
+		poolScheduler.setPoolSize(20);
+		poolScheduler.setThreadNamePrefix("fnf-");
+		poolScheduler.setWaitForTasksToCompleteOnShutdown(false);
+		poolScheduler.initialize();
+		taskRegistrar.setTaskScheduler(poolScheduler);
+	}
+
 }

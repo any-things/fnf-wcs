5bcd4eeb612c4eeef2562bc44fdf9e76f8c251a7
chunseop <chunseop@gmail.com>
2020-08-04 18:31:30 +0900

    층 랙재고 조회 서비스 작업중.

--- src/main/java/operato/fnf/wcs/service/board/GetFloorRackStock.java
+++ src/main/java/operato/fnf/wcs/service/board/GetFloorRackStock.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import operato.fnf.wcs.FnfUtils;
 import operato.fnf.wcs.service.model.BoardCellSum;
 import operato.fnf.wcs.service.model.BoardRackStock;
 import xyz.anythings.base.model.ResponseObj;
@@ -13,23 +14,25 @@ import xyz.anythings.base.service.impl.AbstractLogisService;
 import xyz.elidom.orm.IQueryManager;
 import xyz.elidom.orm.manager.DataSourceManager;
 import xyz.elidom.util.BeanUtil;
+import xyz.elidom.util.DateUtil;
 import xyz.elidom.util.ValueUtil;
 
 @Component
 public class GetFloorRackStock extends AbstractLogisService {
-	public ResponseObj getRackStock(Map<String, Object> params) throws Exception {
-		IQueryManager wmsQueryMgr = BeanUtil.get(DataSourceManager.class).getQueryManager("WMS");
+	public ResponseObj getFloorRackStock(Map<String, Object> params) throws Exception {
 		String floorCd = String.valueOf(params.get("floorCd"));
 		String wcellNo = String.valueOf(params.get("wcellNo"));
 		String buildingTcd = String.valueOf(params.get("buildingTcd"));
 		String date = String.valueOf(params.get("date"));
 		
-//		if (ValueUtil.isEmpty(date)) {
-//			params.put("date", DateUtil.getCurrentDay());
-//		}
+		if (ValueUtil.isEmpty(date)) {
+			params.put("date", DateUtil.getCurrentDay());
+		}
 		
-		List<BoardRackStock> list = wmsQueryMgr.selectListBySqlPath("operato/fnf/wcs/service/wms/rack_stock.sql", 
-				ValueUtil.newMap("floorCd,wcellNo,buildingTcd,date", floorCd,wcellNo,buildingTcd,date), BoardRackStock.class, 0, 0);
+		String sql = FnfUtils.queryCustServiceWithCheck("board_floor_rack_stock");
+		IQueryManager wmsQueryMgr = BeanUtil.get(DataSourceManager.class).getQueryManager("WMS");
+		Map<String, Object> wmsParams = ValueUtil.newMap("floorCd,wcellNo,buildingTcd,date", floorCd,wcellNo,buildingTcd,date);
+		List<BoardRackStock> list = wmsQueryMgr.selectListBySql(sql, wmsParams, BoardRackStock.class, 0, 0);
 		
 		Map<String, BoardCellSum> cellMap = new HashMap<>();
 		for (BoardRackStock obj: list) {
@@ -37,16 +40,29 @@ public class GetFloorRackStock extends AbstractLogisService {
 			if (ValueUtil.isEmpty(cell)) {
 				cell = new BoardCellSum();
 				cellMap.put(obj.getLocation(), cell);
+				cell.setCapacity((float)obj.getSpaceCbm());
+				cell.setUsed(obj.getUsedRate());	// 셀 사용율을 함수내에서 계산해줌.
+			} else {
+				cell.setCapacity(cell.getCapacity() + obj.getSpaceCbm());
+				cell.setUsed(cell.getUsed() + obj.getUsedRate());	// 셀 사용율을 함수내에서 계산해줌.
 			}
 			if (ValueUtil.isNotEmpty(obj.getSkuCd()) && !cell.getSkuCds().contains(obj.getSkuCd())) {
 				cell.addSkuCd(obj.getSkuCd());
 			}
-			cell.setCapacity(cell.getCapacity() + obj.getSpaceCbm());
-			cell.setUsed(cell.getUsed() + obj.getUsedRate());	// 셀 사용율을 계산해줌.
 		}
+		
+		String skuCntSql = FnfUtils.queryCustServiceWithCheck("board_floor_rack_sku_cnt");
+		Float totalSkuCnt = wmsQueryMgr.selectBySql(skuCntSql, wmsParams, Float.class);
+		
+		String usedRateSql = FnfUtils.queryCustServiceWithCheck("board_floor_rack_used_rate");
+		Float totalUsedRate = wmsQueryMgr.selectBySql(usedRateSql, wmsParams, Float.class);
+		
+		
 		
 		Map<String, Object> values = new HashMap<>();
 		values.put("cells", cellMap.values());
+		values.put("totalSkuCnt", totalSkuCnt);
+		values.put("totalUsedRate", totalUsedRate);
 		ResponseObj resp = new ResponseObj();
 		resp.setItems(list);
 		resp.setValues(values);

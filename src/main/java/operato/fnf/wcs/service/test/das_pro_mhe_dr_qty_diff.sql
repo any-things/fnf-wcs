3ba133e4fd810c492b4fa40f364b3af449a82687
chunseop <chunseop@gmail.com>
2020-08-05 10:02:44 +0900

    Update das_pro_mhe_dr_qty_diff.sql

--- src/main/java/operato/fnf/wcs/service/test/das_pro_mhe_dr_qty_diff.sql
+++ src/main/java/operato/fnf/wcs/service/test/das_pro_mhe_dr_qty_diff.sql
@@ -1,3 +1,6 @@
+
+-- 20200731: 1001537503, 1001536778, 1001536928
+
 SELECT
 	*
 FROM
@@ -8,31 +11,96 @@ FROM
 	FROM
 		(
 		SELECT
+			work_unit,
 			shipto_id,
 			item_cd,
 			SUM(pick_qty) AS pick_qty
 		FROM
 			mhe_dr
 		WHERE
-			work_unit = '1001532793'
+			work_unit IN ('1001537503', '1001536778', '1001536928')
 		GROUP BY
+			work_unit,
 			shipto_id,
 			item_cd) DR,
 		(
 		SELECT
+			work_unit,
 			shipto_id,
 			item_cd,
 			SUM(cmpt_qty) AS cmpt_qty
 		FROM
 			mhe_box
 		WHERE
-			work_unit = '1001532793'
+			work_unit IN ('1001537503', '1001536778', '1001536928')
 			AND del_yn != 'Y'
 		GROUP BY
+			work_unit,
 			shipto_id,
 			item_cd) br
 	WHERE
-		dr.shipto_id = br.shipto_id
+		dr.work_unit = br.work_unit
+		AND dr.shipto_id = br.shipto_id
 		AND dr.item_cd = br.item_cd) aa
 WHERE
 	pick_qty != cmpt_qty
+ORDER BY
+	work_unit;
+  
+
+
+
+
+
+
+
+SELECT
+	*
+FROM
+	(
+	SELECT
+		DR.*,
+		br.cmpt_qty
+	FROM
+		(
+		SELECT
+			work_date,
+			work_unit,
+			shipto_id,
+			item_cd,
+			SUM(pick_qty) AS pick_qty
+		FROM
+			mhe_dr
+		WHERE
+			work_date = '20200731'
+		GROUP BY
+			work_date,
+			work_unit,
+			shipto_id,
+			item_cd) DR,
+		(
+		SELECT
+			work_date,
+			work_unit,
+			shipto_id,
+			item_cd,
+			SUM(cmpt_qty) AS cmpt_qty
+		FROM
+			mhe_box
+		WHERE
+			work_date = '20200731'
+			AND del_yn != 'Y'
+		GROUP BY
+			work_date,
+			work_unit,
+			shipto_id,
+			item_cd) br
+	WHERE
+		dr.work_date = br.work_date
+		AND dr.work_unit = br.work_unit
+		AND dr.shipto_id = br.shipto_id
+		AND dr.item_cd = br.item_cd) aa
+WHERE
+	pick_qty != cmpt_qty
+ORDER BY
+	work_unit;

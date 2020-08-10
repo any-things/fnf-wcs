46f6edbf33cf12d751a5c6c47153875c2eaaa2eb
chunseop <chunseop@gmail.com>
2020-08-10 17:59:19 +0900

    Create VolumnByDay.java

---
+++ src/main/java/operato/fnf/wcs/service/model/VolumnByDay.java
@@ -0,0 +1,18 @@
+package operato.fnf.wcs.service.model;
+
+public class VolumnByDay {
+	private String workDate;
+	private String doneQty;
+	public String getWorkDate() {
+		return workDate;
+	}
+	public void setWorkDate(String workDate) {
+		this.workDate = workDate;
+	}
+	public String getDoneQty() {
+		return doneQty;
+	}
+	public void setDoneQty(String doneQty) {
+		this.doneQty = doneQty;
+	}
+}

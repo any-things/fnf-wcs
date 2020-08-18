f5584051ca27211d8c3c42b237062f2b56920dbc
chunseop <chunseop@gmail.com>
2020-08-18 18:54:41 +0900

    bcr

---
+++ src/main/java/operato/fnf/wcs/entity/DpsBcrIfData.java
@@ -0,0 +1,92 @@
+package operato.fnf.wcs.entity;
+
+import xyz.elidom.dbist.annotation.Column;
+import xyz.elidom.dbist.annotation.PrimaryKey;
+import xyz.elidom.dbist.annotation.GenerationRule;
+import xyz.elidom.dbist.annotation.Table;
+
+@Table(name = "dps_bcr_if_data", idStrategy = GenerationRule.UUID)
+public class DpsBcrIfData extends xyz.elidom.orm.entity.basic.ElidomStampHook {
+	/**
+	 * SerialVersion UID
+	 */
+	private static final long serialVersionUID = 652782250347455504L;
+
+	@PrimaryKey
+	@Column (name = "id", nullable = false, length = 40)
+	private String id;
+
+	@Column (name = "proc_yn", length = 1)
+	private String procYn;
+
+	@Column (name = "waybill_no", length = 64)
+	private String waybillNo;
+
+	@Column (name = "scan_seq")
+	private Integer scanSeq;
+
+	@Column (name = "box_no", length = 64)
+	private String boxNo;
+
+	@Column (name = "status", length = 32)
+	private String status;
+
+	@Column (name = "error_msg", length = 1024)
+	private String errorMsg;
+  
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getProcYn() {
+		return procYn;
+	}
+
+	public void setProcYn(String procYn) {
+		this.procYn = procYn;
+	}
+
+	public String getWaybillNo() {
+		return waybillNo;
+	}
+
+	public void setWaybillNo(String waybillNo) {
+		this.waybillNo = waybillNo;
+	}
+
+	public Integer getScanSeq() {
+		return scanSeq;
+	}
+
+	public void setScanSeq(Integer scanSeq) {
+		this.scanSeq = scanSeq;
+	}
+
+	public String getBoxNo() {
+		return boxNo;
+	}
+
+	public void setBoxNo(String boxNo) {
+		this.boxNo = boxNo;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}	
+}
---
+++ src/main/java/operato/fnf/wcs/service/bcr/BcrBarcodeProcess.java
@@ -0,0 +1,55 @@
+package operato.fnf.wcs.service.bcr;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import operato.fnf.wcs.FnfUtils;
+import operato.fnf.wcs.entity.DpsBcrIfData;
+import operato.fnf.wcs.entity.DpsJobInstance;
+import xyz.anythings.base.model.ResponseObj;
+import xyz.anythings.base.service.impl.AbstractLogisService;
+import xyz.elidom.exception.server.ElidomValidationException;
+import xyz.elidom.util.BeanUtil;
+import xyz.elidom.util.ValueUtil;
+
+@Component
+public class BcrBarcodeProcess extends AbstractLogisService {
+	private final String STATUS_FINISH = "F";
+	public ResponseObj bcrBarcodeProcess(Map<String, Object> params) throws Exception {
+		@SuppressWarnings("unchecked")
+		List<LinkedHashMap<String, Object>> temp = (List<LinkedHashMap<String, Object>>)params.get("list");
+		List<DpsBcrIfData> list = FnfUtils.parseObjList(DpsBcrIfData.class, temp);
+		
+		for (DpsBcrIfData obj: list) {
+			Map<String, Object> dataParams = new HashMap<>();
+			dataParams.put("waybillNo", obj.getWaybillNo());
+			BeanUtil.get(BcrBarcodeProcess.class).bcrBarcodeProcess(dataParams);
+		}
+		
+		return new ResponseObj();
+	}
+	
+	public void processData(DpsBcrIfData obj) throws Exception {
+		try {
+			DpsJobInstance dpsJobInstance = queryManager.selectByCondition(DpsJobInstance.class, obj.getId());
+			
+			if (ValueUtil.isEmpty(dpsJobInstance)) {
+				throw new ElidomValidationException("BcrBarcodeRead: boxId["+ obj.getWaybillNo() +"] not found~~");
+			}
+			dpsJobInstance.setBcrStatus(STATUS_FINISH);
+			queryManager.update(dpsJobInstance, "bcrStatus");
+			
+			obj.setProcYn("Y");
+			queryManager.update(obj, "procYn");
+		} catch(Exception e) {
+			logger.error("BcrBarcodeProcessJob error~~", e);
+			obj.setProcYn("E");
+			obj.setErrorMsg(e.getMessage());
+			queryManager.update(obj, "procYn");
+		}
+	}
+}

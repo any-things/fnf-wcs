7cf4793efa3860273a8f9920ea899cc38e417245
jyp220 <jyp@hatiolab.com>
2020-08-18 21:44:47 +0900

    fix

--- src/main/java/operato/logis/sms/rest/SmsTrackingController.java
+++ src/main/java/operato/logis/sms/rest/SmsTrackingController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;

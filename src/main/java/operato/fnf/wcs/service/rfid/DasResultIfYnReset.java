0b2798365f6ab9555c74d5a4d0ad3ca1cf04c5a1
chunseop <chunseop@gmail.com>
2020-08-27 00:58:57 +0900

    Update DasResultIfYnReset.java

--- src/main/java/operato/fnf/wcs/service/rfid/DasResultIfYnReset.java
+++ src/main/java/operato/fnf/wcs/service/rfid/DasResultIfYnReset.java
@@ -1,11 +1,13 @@
 package operato.fnf.wcs.service.rfid;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
 import operato.fnf.wcs.entity.WcsMheBox;
+import xyz.anythings.base.entity.JobBatch;
 import xyz.anythings.base.model.ResponseObj;
 import xyz.anythings.sys.service.AbstractQueryService;
 import xyz.elidom.dbist.dml.Query;
@@ -14,9 +16,23 @@ import xyz.elidom.dbist.dml.Query;
 public class DasResultIfYnReset extends AbstractQueryService {
 	public ResponseObj dasResultIfYnReset(Map<String, Object> params) throws Exception {
 		
+		Query batchConds = new Query();
+		batchConds.addFilter("status", "RUN");
+		List<JobBatch> jobBatches = queryManager.selectList(JobBatch.class, batchConds);
+		
+		if (jobBatches.size() == 0) {
+			return new ResponseObj();
+		}
+		
+		List<String> ids = new ArrayList<>();
+		for (JobBatch obj: jobBatches) {
+			ids.add(obj.getWmsBatchNo());
+		}
+		
 		Query conds = new Query();
 		conds.addFilter("ifYn", "E");
 		conds.addFilter("delYn", "N");
+		conds.addFilter("workUnit", "in", ids);
 		List<WcsMheBox> wcsMheBoxList = queryManager.selectList(WcsMheBox.class, conds);
 		for (WcsMheBox obj: wcsMheBoxList) {
 			obj.setIfYn("N");

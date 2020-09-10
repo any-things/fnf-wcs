b75af009af46a6333d8ef53fcb6e8918457542f0
chunseop <chunseop@gmail.com>
2020-09-10 23:25:34 +0900

    Update board_dps_process.sql

--- src/main/java/operato/fnf/wcs/service/test/board_dps_process.sql
+++ src/main/java/operato/fnf/wcs/service/test/board_dps_process.sql
@@ -1,1 +1,43 @@
+SELECT
+	SUM(cmpt_qty) AS cmpt_qty
+FROM
+	(
+	SELECT
+		item_cd,
+		SUM(cmpt_qty) AS cmpt_qty
+	FROM
+		dps_job_instances dji
 	WHERE
+		work_date = '20200820'
+		AND status = 'E'
+	GROUP BY
+		item_cd) aa;
+
+
+
+
+
+
+
+SELECT
+	SUM(cmpt_qty)
+FROM
+	(
+	SELECT
+		dji.item_cd,
+		cmpt_qty AS cmpt_qty
+	FROM
+		(
+		SELECT
+			item_cd,
+			SUM(cmpt_qty) AS cmpt_qty
+		FROM
+			dps_job_instances dji
+		WHERE
+			work_date = '20200820'
+			AND status = 'E'
+		GROUP BY
+			item_cd) dji,
+		sku ss
+	WHERE
+		dji.item_cd = ss.sku_cd) aa;

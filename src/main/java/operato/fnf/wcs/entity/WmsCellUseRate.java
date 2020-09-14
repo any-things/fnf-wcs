f273edf96bf8f928cda94daae33b62efa78d89b5
chunseop <chunseop@gmail.com>
2020-09-14 17:52:59 +0900

    Create WmsCellUseRate.java

---
+++ src/main/java/operato/fnf/wcs/entity/WmsCellUseRate.java
@@ -0,0 +1,55 @@
+package operato.fnf.wcs.entity;
+
+import xyz.elidom.dbist.annotation.Table;
+
+@Table(name = "vms_cell_use_rate")
+public class WmsCellUseRate extends xyz.elidom.orm.entity.basic.AbstractStamp {
+	/**
+	 * SerialVersion UID
+	 */
+	private static final long serialVersionUID = 5380911312111807384L;
+	
+	private String whCd;
+	private String buildingTcd;
+	private String floorTcd;
+	private Integer totLocCnt;
+	private Integer useLocCnt;
+	private Float rtUseLoc;
+	
+	public String getWhCd() {
+		return whCd;
+	}
+	public void setWhCd(String whCd) {
+		this.whCd = whCd;
+	}
+	public String getBuildingTcd() {
+		return buildingTcd;
+	}
+	public void setBuildingTcd(String buildingTcd) {
+		this.buildingTcd = buildingTcd;
+	}
+	public String getFloorTcd() {
+		return floorTcd;
+	}
+	public void setFloorTcd(String floorTcd) {
+		this.floorTcd = floorTcd;
+	}
+	public Integer getTotLocCnt() {
+		return totLocCnt;
+	}
+	public void setTotLocCnt(Integer totLocCnt) {
+		this.totLocCnt = totLocCnt;
+	}
+	public Integer getUseLocCnt() {
+		return useLocCnt;
+	}
+	public void setUseLocCnt(Integer useLocCnt) {
+		this.useLocCnt = useLocCnt;
+	}
+	public Float getRtUseLoc() {
+		return rtUseLoc;
+	}
+	public void setRtUseLoc(Float rtUseLoc) {
+		this.rtUseLoc = rtUseLoc;
+	}
+}

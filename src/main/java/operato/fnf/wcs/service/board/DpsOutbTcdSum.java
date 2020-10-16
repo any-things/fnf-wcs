1775131f07671e930cfcd041aadb4cd93d15d8bd
chunseop <chunseop@gmail.com>
2020-10-16 18:34:55 +0900

    dps board

---
+++ src/main/java/operato/fnf/wcs/service/board/DpsOutbTcdSum.java
@@ -0,0 +1,44 @@
+package operato.fnf.wcs.service.board;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import operato.fnf.wcs.FnfUtils;
+import operato.fnf.wcs.service.model.DpsOutbWaybill;
+import xyz.anythings.base.model.ResponseObj;
+import xyz.anythings.sys.service.AbstractQueryService;
+import xyz.elidom.util.DateUtil;
+import xyz.elidom.util.ValueUtil;
+
+@Component
+public class DpsOutbTcdSum extends AbstractQueryService {
+	public ResponseObj dpsBatchAutoMerge(Map<String, Object> params) throws Exception {
+		// TODO FNF_IF.MPS_PACKING_CMPT.OUTB_TCD, MPS_EXPRESS_WAYBILL_PRINT
+
+		String date = String.valueOf(params.get("date"));
+		if (ValueUtil.isEmpty(date)) {
+			params.put("date", DateUtil.getCurrentDay());
+		}
+		
+		String byBrandSql = FnfUtils.queryCustServiceWithCheck("board_dps_outb_brand_summary");
+		List<DpsOutbWaybill> dpsOutbs = queryManager.selectListBySql(byBrandSql, params, DpsOutbWaybill.class, 0, 10000);
+		
+		String byOutbTcdSql = FnfUtils.queryCustServiceWithCheck("board_dps_outb_tcd_summary");
+		List<DpsOutbWaybill> dpsOutbTcds = queryManager.selectListBySql(byOutbTcdSql, params, DpsOutbWaybill.class, 0, 10000);
+		
+		Map<String, DpsOutbWaybill> outbTcds = new HashMap<>();
+		for (DpsOutbWaybill obj: dpsOutbTcds) {
+			outbTcds.put(obj.getWaybillNo(), obj);
+		}
+		
+		
+		
+		//Map<String, Object> result = new HashMap<>();
+		ResponseObj resp = new ResponseObj();
+		resp.setItems(dpsOutbs);
+		return resp;
+	}
+}
---
+++ src/main/java/operato/fnf/wcs/service/model/DpsOutbWaybill.java
@@ -0,0 +1,68 @@
+package operato.fnf.wcs.service.model;
+
+public class DpsOutbWaybill {
+	private String boxInputSeq;
+	private String waybillNo;
+	private String strrId;
+	private String strrNm;
+	private String outbTcd;
+	private String refNo;
+	private String status;
+	private String updatedAt;
+	private Integer cmptQty;
+	
+	public String getBoxInputSeq() {
+		return boxInputSeq;
+	}
+	public void setBoxInputSeq(String boxInputSeq) {
+		this.boxInputSeq = boxInputSeq;
+	}
+	public String getWaybillNo() {
+		return waybillNo;
+	}
+	public void setWaybillNo(String waybillNo) {
+		this.waybillNo = waybillNo;
+	}
+	public String getStrrId() {
+		return strrId;
+	}
+	public void setStrrId(String strrId) {
+		this.strrId = strrId;
+	}
+	public String getStrrNm() {
+		return strrNm;
+	}
+	public void setStrrNm(String strrNm) {
+		this.strrNm = strrNm;
+	}
+	public String getOutbTcd() {
+		return outbTcd;
+	}
+	public void setOutbTcd(String outbTcd) {
+		this.outbTcd = outbTcd;
+	}
+	public String getRefNo() {
+		return refNo;
+	}
+	public void setRefNo(String refNo) {
+		this.refNo = refNo;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public Integer getCmptQty() {
+		return cmptQty;
+	}
+	public void setCmptQty(Integer cmptQty) {
+		this.cmptQty = cmptQty;
+	}
+}
